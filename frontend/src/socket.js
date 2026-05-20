8a5714feea7e70114a655c2beb71bc06abe8babb
savemoneyofficialapp <savemoneyofficial.app@gmail.com>
2026-05-20 15:01:01 +0530

    final socket fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 const API = "https://save-money-yyv1.onrender.com";
 
-const socket = io(
-  `${API}`
-);
+const socket = io(API, {
+  transports: ["websocket"],
+});
 
 export default socket;
