f03b7860f07952c82960b9c36f4fe4156944ad08
savemoneyofficialapp <savemoneyofficial.app@gmail.com>
2026-05-20 16:49:49 +0530

    fix login backend url

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://YOUR-VERCEL-URL.vercel.app"
+    "https://save-money-indol.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "authorization"]
