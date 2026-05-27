6c2e320966d8837a9bdc6e1d87f992e8629e68a3
savemoneyofficialapp <savemoneyofficial.app@gmail.com>
2026-05-27 23:04:19 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -806,10 +806,17 @@ const createNotification = async (email, message) => {
 };
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
-    user: process.env.EMAIL_USER,
+   user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
