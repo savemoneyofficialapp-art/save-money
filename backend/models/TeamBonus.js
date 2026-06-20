53526160de69536b9eb47165398b65cbce385c6f
savemoneyofficialapp-art <savemoneyofficial.app@gmail.com>
2026-06-20 18:20:06 +0530

    Update TeamBonus.js

--- backend/models/TeamBonus.js
+++ backend/models/TeamBonus.js
@@ -1,105 +1,25 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
+const schema = new mongoose.Schema({
+  email: String,
 
-const schema=new mongoose.Schema({
+  challengeStart: Date,
+  directCount: { type: Number, default: 0 },
 
+  isActive: { type: Boolean, default: false },
+  isFailed: { type: Boolean, default: false },
 
-email:String,
+  wallet: { type: Number, default: 0 },
 
-
-
-challengeStart:{
-type:Date,
-default:Date.now
-},
-
-
-
-directCount:{
-type:Number,
-default:0
-},
-
-
-
-
-isActive:{
-type:Boolean,
-default:false
-},
-
-
-
-isFailed:{
-type:Boolean,
-default:false
-},
-
-
-
-wallet:{
-type:Number,
-default:0
-},
-
-
-
-
-activatedAt:Date,
-
-deactivatedAt:Date,
-
-updatedAt:Date,
-
-
-
-
-
-history:[
-
-
-{
-
-fromUser:String,
-
-
-fromEmail:String,
-
-
-
-level:Number,
-
-
-
-amount:Number,
-
-
-
-status:{
-type:String,
-default:"Paid"
-},
-
-
-
-date:{
-type:Date,
-default:Date.now
-}
-
-
-}
-
-
-]
-
-
+  history: [
+    {
+      fromUser: String,
+      level: Number,
+      amount: Number,
+      date: Date,
+      status: String
+    }
+  ]
 });
 
-
-
-module.exports=
-mongoose.model(
-"TeamBonus",
-schema
-);
+module.exports = mongoose.model("TeamBonus", schema);
