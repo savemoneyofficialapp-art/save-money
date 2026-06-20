9e2b449bda2cb239d217c5e804a123db212e11c6
savemoneyofficialapp-art <savemoneyofficial.app@gmail.com>
2026-06-20 18:18:10 +0530

    Update PerformanceBonus.js

--- backend/models/PerformanceBonus.js
+++ backend/models/PerformanceBonus.js
@@ -1,99 +1,49 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
+const schema = new mongoose.Schema({
+  email: String,
 
-const schema=new mongoose.Schema({
+  challengeStart: Date,
 
+  totalDirect: {
+    type: Number,
+    default: 0
+  },
 
-email:String,
+  isActive: {
+    type: Boolean,
+    default: false
+  },
 
+  isFailed: {
+    type: Boolean,
+    default: false
+  },
 
-challengeStart:{
-type:Date,
-default:Date.now
-},
+  thisMonthBonus: {
+    type: Number,
+    default: 0
+  },
 
-deadline:Date,
+  lastMonthBonus: {
+    type: Number,
+    default: 0
+  },
 
+  totalBonus: {
+    type: Number,
+    default: 0
+  },
 
-directCount:{
-type:Number,
-default:0
-},
-
-
-
-required:{
-type:Number,
-default:10
-},
-
-
-
-isActive:{
-type:Boolean,
-default:false
-},
-
-
-
-isFailed:{
-type:Boolean,
-default:false
-},
-
-
-
-wallet:{
-type:Number,
-default:0
-},
-
-
-
-activatedAt:Date,
-
-failedAt:Date,
-
-
-
-history:[
-
-{
-
-fromUser:String,
-
-fromEmail:String,
-
-
-amount:Number,
-
-
-planYears:Number,
-
-
-date:{
-type:Date,
-default:Date.now
-},
-
-
-status:{
-type:String,
-default:"Paid"
-}
-
-}
-
-]
-
-
-
+  history: [
+    {
+      fromUser: String,
+      amount: Number,
+      plan: String,
+      date: Date,
+      status: String
+    }
+  ]
 });
 
-
-
-module.exports=
-mongoose.model(
-"PerformanceBonus",
-schema
-);
+module.exports = mongoose.model("PerformanceBonus", schema);
