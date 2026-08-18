3af7ed8ce7a2d1d0b991fbbf679e299bdbd13008
savemoneyofficialapp-art <savemoneyofficial.app@gmail.com>
2026-08-18 20:13:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7467,7 +7467,7 @@ app.get("/admin/withdraw-requests", async (req, res) => {
 });
 
 
-App.get("/investment-certificate/:id", async (req, res) => {
+app.get("/investment-certificate/:id", async (req, res) => {
   try {
     const investment = await Investment.findById(req.params.id);
 
