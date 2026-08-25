cfbf9ae26c9d4de1a96b477c9f8f842e36462734
savemoneyofficialapp-art <savemoneyofficial.app@gmail.com>
2026-08-25 15:56:53 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1809,11 +1809,6 @@ const keyframes = `
   0% { transform: translate3d(0, 0, 0); }
   100% { transform: translate3d(-100%, 0, 0); }
 }
-@keyframes shutterProgress {
-  0% { width: 0%; }
-  50% { width: 70%; }
-  100% { width: 100%; }
-}
 `;
 try {
   styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
