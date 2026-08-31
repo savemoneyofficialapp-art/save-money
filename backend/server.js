2c97be43914b7b8be2bcbdd461911be36d33143d
savemoneyofficialapp-art <savemoneyofficial.app@gmail.com>
2026-08-31 23:14:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9416,6 +9416,6 @@ app.use((err, req, res, next) => {
 // ✅ server.listen MUST be outside io.on
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);  
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
