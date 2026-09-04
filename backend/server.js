16fad8a5c0725770218452862a8029350127bd1c
savemoneyofficialapp-art <savemoneyofficial.app@gmail.com>
2026-09-05 05:02:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10056,7 +10056,7 @@ cron.schedule('0 12,17 * * *', async () => {
 // ===================================================
 // ONETIME DAILY RETURN AUTOMATION (প্রতিদিন রাত ১২:০০ টা IST)
 // ===================================================
-cron.schedule("0 0 * * *", async () => {
+cron.schedule("5 7 * * *", async () => {
   console.log("⏰ Running OneTime Daily Return Cron Job at 12:00 AM IST...");
 
   try {
